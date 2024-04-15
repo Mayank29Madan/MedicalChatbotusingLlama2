063e93cb098722708f20b33e48c5e4b4075f2afe
Mayank <mayankmadan9910127960@gmail.com>
2024-04-15 21:39:24 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Mayank Madan',
+    author_email= 'mayankmadan9910127960@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
